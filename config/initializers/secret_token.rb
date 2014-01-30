5ad16838582ee737c27ef032cda2eceaae6ca93e
Kwadwo Owusu <k.owusu@us.nature.com>
2014-01-30 11:56:10 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DonationsApp::Application.config.secret_token = '2929c1ca2f96194d00c7eb8022a310f1cf446b8452be045d3e7a7e774fa1439d9b92234a3b34ecad1ac56bae514375929b5d11df9da91c65971e05bef343550b'
