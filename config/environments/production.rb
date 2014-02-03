aa5b124ff7baa4fcde1abfb174f3d5c889b9bb64
Kwadwo Owusu <k.owusu@us.nature.com>
2014-02-03 03:40:25 -0500

    added all js to procompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ DonationsApp::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-    config.assets.precompile += %w( jquery.js, jquery-ui.js, bootstrap.js )
+    config.assets.precompile += %w( jquery.js jquery-ui.js bootstrap.js jquery_ujs.js general.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
